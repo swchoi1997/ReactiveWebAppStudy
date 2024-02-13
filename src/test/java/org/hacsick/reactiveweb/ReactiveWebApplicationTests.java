ad7b52d4fd9f5adc0a01d6a1a9de62c6d76d5cfa
SangWon <swchoi1997@naver.com>
2024-02-13 12:38:56 +0900

    init-commit

---
+++ src/test/java/org/hacsick/reactiveweb/ReactiveWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hacsick.reactiveweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
