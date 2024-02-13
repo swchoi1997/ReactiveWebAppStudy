1d80926e7d4525b857f7acd67ee109fab9e15592
최상원 <swchoi1@swm.ai>
2024-02-13 13:38:07 +0900

    flux 간단 예제 추가

--- src/main/java/org/hacsick/reactiveweb/HelloReactor.java
+++ src/main/java/org/hacsick/reactiveweb/HelloReactor.java
@@ -1,11 +1,28 @@
 package org.hacsick.reactiveweb;
 
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.Locale;
+
+@Slf4j
 public class HelloReactor {
 
     public static void main(String[] args) {
+        HelloReactor.fluxEx();
+    }
+
+
+    private static void monoEx() {
         Mono.just("Hello Reactor")
                 .subscribe(System.out::println);
+    }
+
+    private static void fluxEx() {
+        Flux<String> sequence = Flux.just("Hello", "Reactor");
+        sequence.map(data -> data.toLowerCase())
+                .subscribe(data -> log.info(data));
+
     }
 }
